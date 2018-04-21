ad89bb33860f44f525e79844bacb22e3146f62ed
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-21 21:07:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function getFirstSelector(selectors){
   return document.querySelector(selectors)
 }
+
+function nestedTarget(){
+
+}
+
+function increaseRankBy(n){
+
+}
+
+function deepestChild(){
+  
+}
