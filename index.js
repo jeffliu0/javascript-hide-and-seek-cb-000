d570f99a81bff71e736e3be4206a904ac19c0252
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-21 21:03:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 function getFirstSelector(selectors){
-  if(selectors.length == 0){
-    return console.error("Selectors is empty")
-  }
-  return document.getElementsByTagName('div')[0]
+  return querySelector(selectors)
 }
